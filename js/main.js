8776e1ed2fa5df426fd1cdd89801bafc9bcd7fb5
Umarbek Yoqubov <umarbekyoqubov39@gmail.com>
2026-04-06 19:26:31 +0300

    js masalalar

--- js/main.js
+++ js/main.js
@@ -14,9 +14,31 @@
 //  Har bir vaqt quyidagicha beriladi:
 // H M S
 
-let h1=20,m1=1,s1=60
-let h2=10,m2=2,s2=120
-let t1= h1 * 7200 + m1 * 60 + s1
-let t2=h2 * 3600 +m2  * 60 + s2
-let farq=t1-t2
-console.log(farq);
+// let h1=20,m1=1,s1=60
+// let h2=10,m2=2,s2=120
+// let t1= h1 * 7200 + m1 * 60 + s1
+// let t2=h2 * 3600 +m2  * 60 + s2
+// let farq=t1-t2
+// console.log(farq);
+
+// ================================================================================================================
+
+// 3-masala
+// masala sharti:senda bita musbat son bor va sen buni 17% olib log g CHIQAR
+
+// let son=40
+// let foiz=son % 17
+// console.log(foiz);
+
+// ================================================================================================================
+// 4-masla
+// masala sharti:son toq yoki juft ekanligini aniqlan
+
+let toqJUft=+prompt(`ixtiyori son kiritilsin`)
+let natija=toqJUft % 2 ===1
+console.log(natija);
+
+// true=toq songa
+// false= juft songa
+
+
