e932c68022aa020e2c3795622c1b45a3af39be63
Umarbek Yoqubov <umarbekyoqubov39@gmail.com>
2026-04-06 18:30:34 +0300

    js masalallar

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// ================================================================================================================
+
+// 1-masla]
+// masala sharti 
+// foydalanovchi ikki xonalai son kiriti sen uni onliklar xonasida gi sonini oib console log ga chiqar
+
+let sonUser=+prompt(`ixtiyori ikki xonali soni kiriting`)
+let onlik=sonUser/10 |0
+console.log(onlik);
+
