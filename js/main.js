49dae29d7f801f7ba91a775e191047cbb5b67de8
Umarbek Yoqubov <umarbekyoqubov39@gmail.com>
2026-04-06 18:53:48 +0300

    js maslalar

--- js/main.js
+++ js/main.js
@@ -4,7 +4,19 @@
 // masala sharti 
 // foydalanovchi ikki xonalai son kiriti sen uni onliklar xonasida gi sonini oib console log ga chiqar
 
-let sonUser=+prompt(`ixtiyori ikki xonali soni kiriting`)
-let onlik=sonUser/10 |0
-console.log(onlik);
+// let sonUser=+prompt(`ixtiyori ikki xonali soni kiriting`)
+// let onlik=sonUser/10 |0
+// console.log(onlik);
 
+// ==============================================================================================================
+// 2-masla
+// masala sharti 
+//  Har bir vaqt quyidagicha beriladi:
+// H M S
+
+let h1=20,m1=1,s1=60
+let h2=10,m2=2,s2=120
+let t1= h1 * 7200 + m1 * 60 + s1
+let t2=h2 * 3600 +m2  * 60 + s2
+let farq=t1-t2
+console.log(farq);
